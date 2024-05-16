744e376c6a97e12c27ae3eba390f83d8265b467c
francesco <info.francescobianco@gmail.com>
2024-05-16 15:44:07 +0200

    First commit

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
 
 start:
 	@docker build -t wbmserver .
-	@docker run -v $${PWD}/tmp:/var/wbm/data wbmserver
+	@docker run -it --init -v $${PWD}/tmp:/var/wbm/data -p 3000:3000 wbmserver
 
 test-send:
 	@curl -X POST -H "Content-Type: application/json" -d '{"message": "Hello World"}' http://localhost:3000/send
--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 
 const wbm = require('wbm');
-const fastify = require('fastify')({
-    logger: true
+const express = require('express')
+const app = express()
+
+app.post('/send', async (req, res) => {
+    const phones = ['393200466987'];
+    const message = 'Good Morning.';
+    await fastify.wbm.send(phones, message);
+
+    res.send('hello world')
 })
 
-fastify.decorate('wbm', wbm);
-fastify.register(require('./routes'))
+app.listen(3000)
 
+/*
 wbm.start().then(async () => {
     fastify.listen({ port: 3000, host: '0.0.0.0' }, function (err, address) {
         if (err) {
@@ -16,3 +23,4 @@ wbm.start().then(async () => {
         fastify.log.info(`Server listening on ${address}`)
     })
 }).catch(err => console.log(err));
+*/
