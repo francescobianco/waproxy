692808ae3850dce7cdde4eda060d94fbd87ce81d
francesco <info.francescobianco@gmail.com>
2024-05-17 18:32:01 +0200

    First commit

--- Makefile
+++ Makefile
@@ -4,4 +4,4 @@ start:
 	@docker run -it --init -v $${PWD}/tmp:/var/wbm/data -p 3000:3000 wbmserver
 
 test-send:
-	@curl -X POST -H "Content-Type: application/json" -d '{"message": "Hello World"}' http://localhost:3000/send
+	@curl -X POST -d "to=WhatsApp Test" -d message=ciao http://localhost:3000/send
--- index.js
+++ index.js
@@ -3,24 +3,17 @@ const wbm = require('wbm');
 const express = require('express')
 const app = express()
 
-app.post('/send', async (req, res) => {
-    const phones = ['393200466987'];
-    const message = 'Good Morning.';
-    await fastify.wbm.send(phones, message);
+app.use(express.urlencoded({ extended: true }));
 
-    res.send('hello world')
+app.post('/send', async (req, res) => {
+    const phones = [ req.body.to ];
+    const message = req.body.message;
+
+    await wbm.send(phones, message);
+
+    res.send('Ok!'+"\n")
 })
 
-app.listen(3000)
-
-/*
 wbm.start().then(async () => {
-    fastify.listen({ port: 3000, host: '0.0.0.0' }, function (err, address) {
-        if (err) {
-            fastify.log.error(err)
-            process.exit(1)
-        }
-        fastify.log.info(`Server listening on ${address}`)
-    })
+    app.listen(3000)
 }).catch(err => console.log(err));
-*/
