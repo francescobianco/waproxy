29bef1082327f03bed48f16d4bb671481a3358a9
francesco <info.francescobianco@gmail.com>
2025-03-02 19:20:57 +0100

    update

--- behaviours/daily-wakeup.js
+++ behaviours/daily-wakeup.js
@@ -1,6 +1,9 @@
 
 module.exports = function(chat, web, cron) {
-    cron.schedule('* * * * *', () => {
-        console.log("TEST")
+    cron.schedule('* * * * *', async () => {
+        const to = '393200466987';
+        const message = 'Bom dia!';
+        const numberId = await chat.getNumberId(to);
+        const sendMessage = await chat.sendMessage(numberId._serialized, message);
     });
 }
