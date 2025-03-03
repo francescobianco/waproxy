a55980552f1536bd925ed45dcc19cf21afe6bb46
francesco <info.francescobianco@gmail.com>
2025-03-03 16:06:58 +0100

    update

--- behaviours/chat-info.js
+++ behaviours/chat-info.js
@@ -3,6 +3,7 @@ module.exports = function(chat, web) {
     chat.on('message', msg => {
         if (msg.body === '/info') {
             console.log("INFO:", msg)
+            console.log("CHAT:", msg.getChat())
             msg.reply(
                 "info\n"
             );
