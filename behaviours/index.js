9dbac00f5cadc660f5e6b4dfb312ff52a4d5dea3
francesco <info.francescobianco@gmail.com>
2025-03-03 12:31:18 +0100

    update

--- behaviours/index.js
+++ behaviours/index.js
@@ -2,10 +2,12 @@ const sayHelloToEveryone = require('./say-hello-to-everyone');
 const replyToPing = require('./reply-to-ping');
 const webSend = require('./web-send');
 const dailyWakeup = require('./daily-wakeup');
+const dailyChessPuzzle = require('./daily-chess-puzzle');
 
 module.exports = function(chat, web, cron) {
     sayHelloToEveryone(chat, web, cron);
     replyToPing(chat, web, cron);
     webSend(chat, web, cron);
     dailyWakeup(chat, web, cron);
+    dailyChessPuzzle(chat, web, cron);
 }
